3a27c71d151e80923dc14e0352aac8d20f1d566d
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2021-12-11 09:35:15 -0800

    created user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Adebayo Rilwan',
+            'email' => 'adebayo@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'),
+            'remember_token' => Str::random(10),
+            'is_admin' => FALSE
+        ]);
+
+        User::create([
+            'name' => 'Adebayo Ajibola',
+            'email' => 'ajibola@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'),
+            'remember_token' => Str::random(10),
+            'is_admin' => TRUE
+        ]);
+    }
+}
