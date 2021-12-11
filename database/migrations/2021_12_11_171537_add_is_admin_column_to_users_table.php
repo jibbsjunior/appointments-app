9d74b673ad3bc553607b95bb130d31b70714433e
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2021-12-11 09:18:14 -0800

    migrate is admin column

--- database/migrations/2021_12_11_171537_add_is_admin_column_to_users_table.php
+++ database/migrations/2021_12_11_171537_add_is_admin_column_to_users_table.php
@@ -14,7 +14,7 @@ class AddIsAdminColumnToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->boolean('is_admin')->default(FALSE);
         });
     }
 
