faa55c93ca06fb7c98f70e9dca321bfe262be464
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2021-12-11 09:39:57 -0800

    created appointments controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AppointmentsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +21,5 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Route::get('/appointments', [AppointmentsController::class, 'index']);
